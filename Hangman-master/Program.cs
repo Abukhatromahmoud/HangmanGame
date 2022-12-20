f395d73db18ba126f0bb91af63e77cb10aaa866b
Abukhatromahmoud <mahmoudabukhatro827@gmail.com>
2022-12-20 11:49:07 +0100

    initial commit

---
+++ Hangman-master/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Configuration;
+using Hangman.View;
+
+namespace Hangman {
+    class Program {
+
+        // Possible words the user has to guess
+        private static readonly string[] HangmanWords = {
+            "HANGMAN", "APPLE", "TOWER", "SMARTPHONE", "PROGRAMMING", "AWKWARD", "BANJO",
+            "DWARVES", "FISHHOOK", "JAZZY", "JUKEBOX", "MEMENTO", "MYSTIFY", "OXYGEN", "PIXEL",
+            "ZOMBIE", "NUMBSKULL", "BAGPIPES", "COMPUTER", "EASTER", "CHRISTMAS", "COFFEE"
+        };
+        
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var visualizer = new TextVisualizer();
+            var game = new Game(visualizer, HangmanWords[random.Next(HangmanWords.Length)]);
+        }
+    }
+}
---
+++ Hangman-master/README.md
@@ -0,0 +1,8 @@
+![Project build status](https://travis-ci.org/MattOnCode/Hangman.svg?branch=master)
+# Hangman
+Console based Hangman game in C#
+
+Another simple console game written in C#. This time, Hangman! Includes a list of possible words to play with.
+"# HangmanGame" 
+"# HangmanGame" 
+"# HangmanGame" 
